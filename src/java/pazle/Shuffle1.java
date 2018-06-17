6f01c24d18e99404dd9a22b54584690819e9c360
KarLLooN <m_loon87@mail.ru>
2018-06-17 17:08:57 +0300

    s

--- src/java/pazle/Shuffle1.java
+++ src/java/pazle/Shuffle1.java
@@ -5,9 +5,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 class Shuffle1 {
     public static void main(String[] args) {
         int x = 3;
-        if (x > 2) {
-            System.out.print("a");
-        }
+
         while (x > 0) {
             x = x - 1;
             System.out.print("-");
